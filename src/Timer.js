4d2d5cfede0d3e428958b4477ecf5d7b99ce60f8
Kevin Huang <yellow.khaos@gmail.com>
2019-02-03 02:03:20 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -10,7 +10,7 @@ class Timer extends Component {
   // add your code here
 
   componentDidMount() {
-    this.interval = setInterval(clockTick, 1000)
+    this.interval = setInterval(this.clockTick, 1000)
   }
 
 
